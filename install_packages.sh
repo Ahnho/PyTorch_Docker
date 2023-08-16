21dd00b62beae7a992cd371bb60720d71a5fd1da
Ahnho <dksgh00@naver.com>
2023-08-16 15:57:20 +0900

    Create install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install the required packages
+pip install opencv-python
+pip install tensorboard
+pip install tensorboardX
+pip install diffdist
+pip install markupsafe==2.0.1
+pip install torch torchaudio
+pip install librosa==0.7.2
+pip install einops
+pip install numba==0.48
+pip install --upgrade librosa
+pip install h5py
+pip install plotly
+pip install spikingjelly
+pip install transformers
+pip install datasets
+pip install pennylane
